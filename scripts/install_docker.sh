900cb1e95df16cd978e0cf5d03d94f486fe12a3d
Daniel Barragán Calderón <d4n13lbc@users.noreply.github.com>
2018-06-16 22:29:13 -0500

    Create install_docker.sh

---
+++ scripts/install_docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# reference: https://docs.docker.com/install/linux/docker-ce/debian/
+# user: pi
+sudo apt-get update -y
+sudo apt-get install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+echo "deb [arch=armhf] https://download.docker.com/linux/debian \
+     $(lsb_release -cs) stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update -y
+sudo apt-get install docker-ce -y
+sudo pip3 install docker-compose
+sudo usermod -aG docker pi
