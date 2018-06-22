bf8cf666d0c14cd0a603f3880ea9b07922bd223c
d4n13lbc <daniel.barragan@correo.icesi.edu.co>
2018-06-21 23:37:05 -0500

    Upload script

---
+++ automation/scripts/install_python_dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# user: pi
+pip3 install virtualenvwrapper --user
+cat <<EOT >> ~/.bashrc
+export WORKON_HOME=~/.virtualenvs
+export PROJECT_HOME=$HOME/Devel
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+export VIRTUALENVWRAPPER_VIRTUALENV=$HOME/.local/bin/virtualenv
+source ~/.local/bin/virtualenvwrapper.sh
+EOT
+source ~/.bashrc
