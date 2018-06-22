121d56c17ecdd8530d7380ef8ba56a2823bdb913
d4n13lbc <daniel.barragan@correo.icesi.edu.co>
2018-06-21 23:58:48 -0500

    Update script

--- automation/scripts/install_python_dev.sh
+++ automation/scripts/install_python_dev.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # user: pi
 pip3 install virtualenvwrapper --user
-cat <<EOT >> ~/.bashrc
+cat <<EOT >> ~/.zshrc
 export WORKON_HOME=~/.virtualenvs
 export PROJECT_HOME=$HOME/Devel
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
 export VIRTUALENVWRAPPER_VIRTUALENV=$HOME/.local/bin/virtualenv
 source ~/.local/bin/virtualenvwrapper.sh
 EOT
-source ~/.bashrc
+source ~/.zshrc
