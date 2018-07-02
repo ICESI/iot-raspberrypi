a5d689f2a0f433241e5c77431a9b679bbf6f6f48
d4n13lbc <daniel.barragan@correo.icesi.edu.co>
2018-07-01 23:29:50 -0500

    Add install picamera script

---
+++ automation/scripts/install_picamera.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# user: pi
+
+echo "Enabling raspicam driver on video0"
+sudo rpi-update
+sudo modprobe bcm2835-v4l2
+echo "Installing motion..."
+sudo apt install motion -y
+echo "Updating configurations..."
+sudo cp ../templates/motion.conf /etc/motion/motion.conf
+sudo chmod 644 /etc/motion/motion.conf
+sudo cp ../templates/motion /etc/default/motion
+sudo chmod 644 /etc/default/motion
+sudo usermod -aG motion pi
+echo "Restarting service..."
+sudo systemctl enable motion
+sudo systemctl restart motion
--- tutorials/03_camera/README.md
+++
@@ -1,2 +0,0 @@
-sudo rpi-update # to make sure the v4L2 drive is available.  
-sudo modprobe bcm2835-v4l2 # to load it and create /dev/video0
