48ec0df7dfe2d8bcf4875a45b2836730e7b5d4bf
d4n13lbc <daniel.barragan@correo.icesi.edu.co>
2018-07-12 02:23:57 +0000

    Update vim script

--- automation/scripts/install_vim.sh
+++ automation/scripts/install_vim.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # user: pi
+echo "Installing vim..."
 sudo apt install vim -y
+echo "Updating configurations..."
+cp ../templates/vimrc ~/.vimrc
+echo "Installing plugins..."
 mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-cp ../templates/vimrc ~/.vimrc
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
