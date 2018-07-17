f6f621ff26916b03af252104b20570d731b58955
d4n13lbc <daniel.barragan@correo.icesi.edu.co>
2018-07-16 22:39:53 -0500

    Update crontab jobs

--- automation/scripts/install_picamera.sh
+++ automation/scripts/install_picamera.sh
@@ -13,6 +13,8 @@ sudo chmod 644 /etc/motion/motion.conf
 sudo cp ../templates/motion /etc/default/motion
 sudo chmod 644 /etc/default/motion
 sudo usermod -aG motion pi
+echo "0 9 * * * root curl http://localhost:8080/0/detection/start -H 'Authorization: Basic cGk6cmFzcGJlcnJ5' > /dev/null" | sudo tee /etc/crontab -a
+echo "0 18 * * * root curl http://localhost:8080/0/detection/pause -H 'Authorization: Basic cGk6cmFzcGJlcnJ5' > /dev/null" | sudo tee /etc/crontab -a
 echo "Restarting service..."
 sudo systemctl enable motion
 sudo systemctl restart motion
