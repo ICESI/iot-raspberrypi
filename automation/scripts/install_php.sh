259c50bc2ee245a9684f49adcb6b84a4e878e4bb
Daniel Barragán Calderón <d4n13lbc@users.noreply.github.com>
2019-04-13 10:02:06 -0500

    Update install_php.sh

--- automation/scripts/install_php.sh
+++ automation/scripts/install_php.sh
@@ -19,7 +19,7 @@ sudo chown -R www-data: .
 sudo mysql_secure_installation
 sudo mysql -uroot -p
 create database wordpress;
-GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' IDENTIFIED BY 'PASSWORD';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' IDENTIFIED BY 'YOURPASSWORD';
 FLUSH PRIVILEGES;
 http://localhost/wp-admin.
 fi
