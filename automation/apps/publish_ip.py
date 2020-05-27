bd9c9f772cb6223ac0c17986fc951fdefa07945b
Daniel Barragán Calderón <d4n13lbc@users.noreply.github.com>
2020-05-27 00:34:38 -0500

    Update publish_ip.py

--- automation/apps/publish_ip.py
+++ automation/apps/publish_ip.py
@@ -1,6 +1,11 @@
 import requests
 import json
 
+# https://crontab.guru/#*/15_*_*_*_*
+# crontab -e
+# m h  dom mon dow   command
+# * 1 * * * python /home/pi/repositories/public-access/publish_ip.py
+
 response = requests.get('https://api.ipify.org?format=json')
 data = json.loads(response.text)
 ip = data["ip"]
