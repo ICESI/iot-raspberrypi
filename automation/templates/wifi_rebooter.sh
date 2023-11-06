f94bed6e8cafb1d165f04c385820f75e8f840e6a
Daniel Barragán Calderón <d4n13lbc@users.noreply.github.com>
2023-11-05 20:46:10 -0500

    Update wifi_rebooter.sh

--- automation/templates/wifi_rebooter.sh
+++ automation/templates/wifi_rebooter.sh
@@ -6,10 +6,20 @@ SERVER=8.8.8.8
 # Only send two pings, sending output to /dev/null
 ping -c2 ${SERVER} > /dev/null
 
+# For the latests version of Raspberry OS
 # If the return code from ping ($?) is not 0 (meaning there was an error)
 if [ $? != 0 ]
 then
     # Restart the wireless interface
-    ifdown --force wlan0
+    /usr/sbin/iw wlan0 link
+    nmcli dev wifi connect YOUR_SSID
     ifup wlan0
 fi
+
+# Old versions of Rasbperry OS
+# if [ $? != 0 ]
+# then
+#     # Restart the wireless interface
+#     ifdown --force wlan0
+#     ifup wlan0
+# fi
